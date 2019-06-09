313bb2e894214b6ab0063cf3435080fee1f5e285
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2019-06-10 00:39:42 +0200

    added new features to landigpage

--- src/AlmostFilmWeb/LandingPage/LandingPage.js
+++ src/AlmostFilmWeb/LandingPage/LandingPage.js
@@ -5,12 +5,10 @@ import NavBar from '../NavBar/NavBar';
 
 
 const urlPopular = 'https://api.themoviedb.org/3/movie/popular?api_key=612ebf63e580831559365d1bc93af503'
-const urlTopRated = 'https://api.themoviedb.org/3/movie/top_rated?api_key=612ebf63e580831559365d1bc93af503'
-const urlUpComing = 'https://api.themoviedb.org/3/movie/upcoming?api_key=612ebf63e580831559365d1bc93af503'
 
 const Description = (props) => { return(
     <div className={Styles.list}>
-      <h1>{props.title}</h1> 
+        <h3>{props.title}</h3>
     </div>
   )
 }
@@ -23,8 +21,6 @@ class LandingPage extends React.Component {
 
         this.state = {
             popularMovies: [],
-            topRatedMovies: [],
-            upComingMovies: [],
         }
     }
 
@@ -32,42 +28,17 @@ class LandingPage extends React.Component {
         fetch(urlPopular)
         .then(response => response.json())
         .then(data => this.setState({popularMovies: data.results}))
-
-        fetch(urlTopRated)
-        .then(response => response.json())
-        .then(data => this.setState({topRatedMovies: data.results}))
-
-        fetch(urlUpComing)
-        .then(response => response.json())
-        .then(data => this.setState({upComingMovies: data.results}))
-      }
+    }
 
     render() {
         return(
-            <div>
-                <h2>Popularne: </h2>
+            <div className = {Styles.LandingPage}>
+                
                 <div className = {Styles.popular}>
+                    <h2>Popularne: </h2>
                     {
                         this.state.popularMovies.map((item) => (
-                            <Description title={item.title} vote_average={item.vote_average} /> 
-                        ))
-                    }
-                </div>
-
-                <h2>Najlepiej Oceniane: </h2>
-                <div className = {Styles.popular}>
-                    {
-                        this.state.topRatedMovies.map((item) => (
-                            <Description title={item.title} vote_average={item.vote_average} /> 
-                        ))
-                    }
-                </div>
-
-                <h2>Nadchodzą: </h2>
-                <div className = {Styles.popular}>
-                    {
-                        this.state.upComingMovies.map((item) => (
-                            <Description title={item.title} vote_average={item.vote_average} /> 
+                            <Description title = {item.title} /> 
                         ))
                     }
                 </div>
