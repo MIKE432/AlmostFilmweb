878e942bcbc62079dc23b7cb3822999004066429
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2019-06-13 22:54:36 +0200

    fixed displaying poster

---
+++ src/AlmostFilmWeb/Login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default class Login extends React.Component {
+    state = {
+        name: '',
+        surname: '',
+
+    }
+
+        
+    
+
+    render() {
+
+    }
+
+}
+
+
